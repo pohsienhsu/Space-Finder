c6df2bda96525239e5150b365dde12df46e4912d
phsu40 <phsu40@gatech.edu>
2023-08-09 21:00:08 -0700

    Create API Gateway Stack and connect with the Lambda function

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -1,10 +1,11 @@
 import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
-
-
-
+import { ApiStack } from "./stacks/ApiStack";
 
 const app = new App();
-new DataStack(app, 'SpaceFinderDataStack');
-new LambdaStack(app, 'SpaceFinderLambdaStack');
+new DataStack(app, "SpaceFinderDataStack");
+const lambdaStack = new LambdaStack(app, "SpaceFinderLambdaStack");
+new ApiStack(app, "SpaceFinderApiStack", {
+  helloLambdaIntegration: lambdaStack.helloLambdaIntegration,
+});
