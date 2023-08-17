60d395b9b3aca108cfbcfec0a95a2a365783dde8
phsu40 <phsu40@gatech.edu>
2023-08-16 19:31:18 -0700

    Integrate Data validation to PostSpaces method

--- src/services/spaces/PostSpaces.ts
+++ src/services/spaces/PostSpaces.ts
@@ -2,11 +2,14 @@ import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { v4 } from "uuid";
+import { validateAsSpaceEntry } from "../shared/Validator";
 
 export async function postSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
     
   const randomId = v4();
   const item = JSON.parse(event.body);
+  item.id = randomId;
+  validateAsSpaceEntry(item);
 
   const result = await ddbClient.send(new PutItemCommand({
     TableName: process.env.TABLE_NAME,
