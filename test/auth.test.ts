ba6a84b6cfa409a997374c4bcfbb1eaa34c1366b
phsu40 <phsu40@gatech.edu>
2023-08-18 14:15:47 -0700

    Get temporary credentials from Cognito Identity Pool

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -3,10 +3,13 @@ import { AuthService } from "./AuthService";
 async function testAuth() {
   const service = new AuthService();
   const loginResult = await service.login(
-    process.env.USERNAME,
-    process.env.PASSWORD
+    "pohsu",
+    "Sesame-0530"
   )
-  console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
+  // console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
+
+  const credentials = await service.generateTemporaryCredentials(loginResult);
+  console.log(credentials);
 }
 
 testAuth();
