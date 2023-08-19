05e82c25f6170bd0e05fcc7e956bcdc59bd27a15
phsu40 <phsu40@gatech.edu>
2023-08-19 14:41:03 -0700

    Create CloudFront distribution to display a dummy UI

--- space-finder-backend/src/infra/stacks/UIDeploymentStack.ts
+++ space-finder-backend/src/infra/stacks/UIDeploymentStack.ts
@@ -1,10 +1,12 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import { getSuffixFromStack } from "../Utils";
 import { existsSync } from "fs";
 import path = require("path");
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Distribution, OriginAccessIdentity } from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 
 
 export class UIDeploymentStack extends Stack {
@@ -27,6 +29,22 @@ export class UIDeploymentStack extends Stack {
     new BucketDeployment(this, 'SpaceFinderUIDeployment', {
       destinationBucket: deploymentBucket,
       sources: [Source.asset(uiDir)]
+    })
+
+    const originIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+    deploymentBucket.grantRead(originIdentity);
+
+    const distribution = new Distribution(this, 'SpaceFinderDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(deploymentBucket, {
+          originAccessIdentity: originIdentity,
+        })
+      }
+    })
+
+    new CfnOutput(this, 'SpaceFinderUrl', {
+      value: distribution.distributionDomainName
     })
   }
 
